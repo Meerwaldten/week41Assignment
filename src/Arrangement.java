5eaa2265fbc431a81d1d88debe19973579a67813
Aleksandar <ale.jov@outlook.com>
2021-10-07 12:02:28 +0200

    Temp product

--- src/Arrangement.java
+++ src/Arrangement.java
@@ -1,50 +1,106 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Arrangement {
-    private Band[] bandArr;
+    static Scanner sc = new Scanner(System.in);
+    private String arrangementName;
+    private Band[] lineup;
     private int numberBands;
     private int availableTickets;
     private double playingTime;
 
-    public Arrangement(Band[] bandArr, int availableTickets) {
-        this.numberBands = bandArr.length;
-        this.bandArr = bandArr;
-        this.availableTickets = availableTickets;
+    public Arrangement() {
     }
 
-    public Band[] getBandArr() {
-        return bandArr;
+    public String getArrangementName() {
+        return arrangementName;
     }
+
+    public void setArrangementName() {
+        System.out.println("Please type in the name of the event:");
+        this.arrangementName = sc.nextLine();
+    }
+
+    public Band[] getlineup() {
+        return lineup;
+    }
+
+    public void setlineup(Band[] lineup) {
+        this.lineup = lineup;
+    }
+
     public int getNumberBands() {
         return numberBands;
     }
+
+    public void setNumberBands() {
+        boolean inputChecker = false;
+        numberBands = 0;
+
+        while(!inputChecker){
+            try{
+                this.numberBands = sc.nextInt();
+                inputChecker = true;
+            }
+            catch (InputMismatchException e){
+                sc.next();
+                System.out.println("Wrong input.");
+                System.out.println("Please type in the number of bands to play at the event.");
+            }
+        }
+        Band[] bandExample = new Band[numberBands];
+        this.lineup = bandExample;
+    }
+
     public int getAvailableTickets() {
         return availableTickets;
     }
+
+    public void setAvailableTickets(int availableTickets) {
+        this.availableTickets = availableTickets;
+    }
+
     public double getPlayingTime() {
         return playingTime;
     }
 
-    public void setBandArr(Band[] bandArr) {
-        this.bandArr = bandArr;
-    }
-    public void setNumberBands(int numberBands) {
-        this.numberBands = numberBands;
-    }
-    public void setAvailableTickets(int availableTickets) {
-        this.availableTickets = availableTickets;
-    }
     public void setPlayingTime(double playingTime) {
         this.playingTime = playingTime;
     }
 
+    public void lineup(int numberBands){
+        for (int i = 0; i < numberBands; i++) {
+            boolean inputChecker = false;
+
+            while(!inputChecker){
+                System.out.println("Type \"0\" to book a band or type \"1\" to select a band who is already contracted:");
+                int userChoice = sc.nextInt();
+                Band bandExample = new Band("",0,0);
+                if(userChoice == 0){
+                    this.lineup[i] = bandExample.createBand();
+                    System.out.println(lineup[i]);
+                    inputChecker = true;
+                }
+                else if(userChoice == 1){
+                    this.lineup[i] = bandExample.chooseBand();
+                    inputChecker = true;
+                }
+                else{
+                    System.out.println("Wrong input.");
+                    System.out.println("Type \"0\" to book a band or type \"1\" to select a band who is already contracted:");
+                    userChoice = sc.nextInt();
+                }
+            }
+        }
+    }
+
     @Override
     public String toString() {
-        return "Arrangement{" +
-                "bandArr=" + Arrays.toString(bandArr) +
-                ", numberBands=" + numberBands +
-                ", availableTickets=" + availableTickets +
-                ", playingTime=" + playingTime +
-                '}';
+        for (Band bands : lineup) {
+            System.out.println(bands);
+
+        }
+        return "";
     }
 }
--- src/Band.java
+++ src/Band.java
@@ -1,24 +1,60 @@
 import java.util.Random;
-
+import java.util.Scanner;
 
 public class Band {
+    static Scanner sc = new Scanner(System.in);
     static Random rd = new Random();
     private String name;
     private int pricePerTicket;
     private double time;
 
-    public Band(String name, double time) {
-        this.pricePerTicket = (int) (Math.random() * (300 - 100)) + 100;
+    public Band(String name, double time, int pricePerTicket) {
+        this.pricePerTicket = pricePerTicket;
         this.name = name;
         this.time = time;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPricePerTicket() {
+        return pricePerTicket;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public Band createBand(){
+        System.out.println("Type in the name of the band:");
+        name = sc.nextLine();
+        System.out.println("Type in how long the band should be on stage:");
+        time = sc.nextInt();
+        System.out.println("The band informs you their price per ticket for booking them will be:");
+        pricePerTicket = (int) (Math.random() * (300-150))+150;
+        System.out.println(pricePerTicket + " DKK.");
+        Band newBand = new Band(name, time, pricePerTicket);
+        sc.nextLine();
+        return newBand;
+    }
+    public Band chooseBand(){
+        System.out.println("The following bands are available for booking:");
+        Band rockBand01 = new Band("System Of A Down", 1,300);
+        Band popBand01 = new Band("Ace of Base", 1.5,250);
+        Band popBand02 = new Band("Dodo And The Dodos",1.25,200);
+        Band popBand03 = new Band("Nik Og Jay", 1,350);
+        Band electronicBand01 = new Band("Major Lazer", 2,400);
+        Band[] listOfHouseBands = {rockBand01, popBand01, popBand02, popBand03, electronicBand01};
+        for (int i = 0; i < listOfHouseBands.length; i++) {
+            System.out.println("type "+(i+1)+" for "+listOfHouseBands[i]);
+        }
+        Band bandChoice = listOfHouseBands[sc.nextInt()-1];
+        return bandChoice;
+    }
+
     @Override
     public String toString() {
-        return "Band{" +
-                "name='" + name + '\'' +
-                ", pricePerTicket=" + pricePerTicket +
-                ", time=" + time +
-                '}';
+        return name + ", who is playing for "+time+" hours, and they cost "+pricePerTicket+" pr ticket.";
     }
 }
